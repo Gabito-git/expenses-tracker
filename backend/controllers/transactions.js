a24edc1f58f0a14c60bd7edbe5bce89f564db4af
Gabriel <luisgabrielarevalo@hotmail.com>
2021-12-15 15:20:42 -0500

    refactor(backend) create controllers folder

--- backend/controllers/transactions.js
+++ backend/controllers/transactions.js
@@ -1,3 +1,15 @@
-const newIncome = () =>{
+const { pool } = require('../db/config');
 
+const newTransaction = async( req, res ) => {
+
+    const { concept, amount, date, category, typename } = req.body
+
+    const newIncome = await pool.query("INSERT INTO transactions ( concept, amount, date, category, typename ) "+
+    "VALUES($1, $2, $3, $4, $5) RETURNING *", [concept, amount, date, category, typename]);
+
+    res.status(201).json({ ...newIncome.rows['0'] });
+}
+
+module.exports={
+    newTransaction
 }
--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -1,8 +1,9 @@
 const { Router } = require('express');
-const { pool } = require('../db/config');
 const { check } = require('express-validator');
+
 const validateFields = require('../middlewares/validarCampos');
 const { allowedTypes } = require('../helpers/db-validators');
+const { newTransaction } = require('../controllers/transactions');
 
 const router = Router();
 
@@ -23,14 +24,6 @@ router.post('/', [
     check('category', 'Category field is mandatory').not().isEmpty(),
     check('typename').custom( c => allowedTypes( c, ['income', 'expense'] ) ),
     validateFields
-],async( req, res ) => {
-
-    const { concept, amount, date, category, typename } = req.body
-
-    const newIncome = await pool.query("INSERT INTO transactions ( concept, amount, date, category, typename ) "+
-    "VALUES($1, $2, $3, $4, $5) RETURNING *", [concept, amount, date, category, typename]);
-
-    res.json({ ...newIncome.rows['0'] });
-} )
+], newTransaction )
 
 module.exports = router;
